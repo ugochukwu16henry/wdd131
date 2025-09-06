58a2cf702cdf3a10494b21f1f5f8ad8d60e3dc19
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-09-06 14:11:42 +0100

    commit

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
