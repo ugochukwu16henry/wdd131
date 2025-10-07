8b4fab8f29bcd52a72850efe9a72aef5c7a1fb6c
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-10-07 17:49:53 +0100

    commit

--- Project/scripts/review.js
+++
@@ -1,9 +0,0 @@
-// Increment review counter in localStorage
-let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
-reviewCount++;
-localStorage.setItem("reviewCount", reviewCount);
-
-// Display it
-document.getElementById(
-  "reviewCounter"
-).textContent = `You have submitted ${reviewCount} review(s).`;
