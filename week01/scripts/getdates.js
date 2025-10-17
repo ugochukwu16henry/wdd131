5dcfbb9678b982a496cdba71f24e47651d63104d
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2025-10-17 21:09:16 +0100

    commit

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -3,3 +3,12 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Get last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+
+const mobileBtn = document.querySelector('.mobile-menu-btn');
+const navList = document.querySelector('nav ul');
+
+mobileBtn.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
